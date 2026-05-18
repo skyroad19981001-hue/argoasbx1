4c9407913d7011650451d647c7bfc65ffaf7229e
skyroad19981001 <skyroad19981001@gmail.com>
2026-05-18 13:43:44 -0400

    argosbx.sh

--- argosbx.sh
+++ argosbx.sh
@@ -2137,7 +2137,7 @@ if [ "$1" = "del" ]; then
 cleandel
 rm -rf sbx_update "$HOME/agsbx" "$HOME/websbx"
 echo "卸载完成"
-echo "欢迎继续使用甬哥侃侃侃ygkkk的Argosbx一键无交互小钢炮脚本💣" && sleep 2
+echo "欢迎再次Argosbx一键脚本" && sleep 2
 echo
 showmode
 exit
